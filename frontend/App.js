49c90d121a8baa4617b108d314f1b72815d3f640
Fred Hong <fredhong.c@gmail.com>
2023-11-26 02:19:28 -0500

    called axios requests

--- frontend/App.js
+++ frontend/App.js
@@ -40,9 +40,7 @@ import { COLORS } from "./constants/theme";
 import Responses from "./pages/Responses";
 import Feedback from "./components/Feedback";
 
-
 const Stack = createNativeStackNavigator();
-
 
 export default function App() {
   registerNNPushToken(15354, "OMR5vhsELb5y4OPcmOkK9z");
